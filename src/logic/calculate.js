2d2411b0e7462473fd275b24cfaef1f8dbf393f3
samroodAli <devsamrood@gmail.com>
2021-09-06 15:08:51 +0530

    Fix:Calculate function argument destructuring

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,3 +1,9 @@
-const calculate = (data = { total: 0, next: '', operation: '' }, button) => ({ data, button });
+import operate, { operations } from './operate';
 
+const calculate = ({ total = 0, next = '', operation = '' }, button) => {
+  if (button in operations) {
+    return operate(total, button);
+  }
+  return { };
+};
 export default calculate;
