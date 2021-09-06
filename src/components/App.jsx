5be25bce8f196bac633067d0c761fdcb6d3254a6
samroodAli <devsamrood@gmail.com>
2021-09-06 14:11:13 +0530

    Render:ButtonPanel in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Display from './Display';
+import ButtonPanel from './ButtonPanel';
 
 class App extends React.PureComponent {
   render() {
     return (
-      <Display />
+      <>
+        <Display />
+        <ButtonPanel />
+      </>
     );
   }
 }
--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from './button';
+import Button from './Button';
 
 class ButtonPanel extends React.PureComponent {
   render() {
