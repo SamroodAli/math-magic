28bbddea0f56c4dba4984c378ef088b555ee8ca6
samroodAli <devsamrood@gmail.com>
2021-09-06 20:10:35 +0530

    Fix:Calculator logic refactor code

--- src/logic/calculate.mjs
+++ src/logic/calculate.mjs
@@ -1,24 +1,19 @@
-import operate, { operations } from './operate.mjs';
+import operate from './operate.mjs';
 
 const calculate = (data, button) => {
   const { total, next, operation } = data;
 
-  if (button === 'AC') {
-    return { total: 0, next: 0, operation: null };
-  }
-
-  const result = operation in operations ? operate(total, next, operation) : next;
+  const result = Number(operate(total, next, operation || '='));
 
   const nextOptions = {
-    '=': result,
     '+/-': next * -1,
     '%': next / 100,
   };
 
   return {
     total: result,
-    next: nextOptions[button] || 0,
-    operation: button in nextOptions ? null : button,
+    next: nextOptions[button] || result,
+    operation: button,
   };
 };
 
--- src/logic/operate.mjs
+++ src/logic/operate.mjs
@@ -1,17 +1,28 @@
 import Big from 'big.js';
 
-const add = (num1, num2) => num1 + num2;
+const add = (num1, num2) => Number(num1) + Number(num2);
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
+const result = (num1, num2) => num2;
+const inverse = (num1) => num1 * -1;
+const modifier = (num1) => num1 / 100;
+const clear = () => 0;
 
-export const operations = {
+const operations = {
   '+': add,
   '-': subtract,
   x: multiply,
   '÷': divide,
+  '=': result,
+  '+/-': inverse,
+  '%': modifier,
+  AC: clear,
 };
 
-const operate = (num1 = 0, num2 = 0, operation) => operations[operation](Big(num1), Big(num2));
+const operate = (num1 = 0, num2 = 0, operation) => {
+  const currentOperation = operations[operation];
+  return currentOperation(new Big(num1), new Big(num2));
+};
 
 export default operate;
