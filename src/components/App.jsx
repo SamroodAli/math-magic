4c9ccf5bb783274a78e7784f396304daebe10f4c
samroodAli <devsamrood@gmail.com>
2021-09-06 20:21:56 +0530

    Interactions:Change App to class component with state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate.js';
+import calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render = (
+    <>
+      <Display />
+      <ButtonPanel />
+    </>
+  )
+}
 export default App;
