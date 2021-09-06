389f01377b602bf842c1b4e8a3afa5944530d487
samroodAli <devsamrood@gmail.com>
2021-09-06 13:44:44 +0530

    Components:Create Display component with default props and prop types

---
+++ src/components/Display.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Display extends React.PureComponent {
+  render() {
+    const { result } = this.props;
+    return (
+      <>{result}</>
+    );
+  }
+}
+
+export default Display;
+
+Display.defaultProps = {
+  result: 0,
+};
+
+Display.propTypes = {
+  result: PropTypes.number,
+};
