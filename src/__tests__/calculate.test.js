b46b5936f9da4f9d8700e3bc4fbf6ec5037dd9ff
samroodAli <devsamrood@gmail.com>
2021-09-22 13:53:48 +0530

    Test: Test calculate function in logic

---
+++ src/__tests__/calculate.test.js
@@ -0,0 +1,32 @@
+import Big from 'big.js';
+import calculate from '../logic/calculate';
+
+test('If button is AC, it return new state', () => {
+  const state = {
+    total: '0',
+    next: '0',
+    operation: '',
+  };
+  const actual = calculate(state, 'AC');
+  expect(actual).toEqual({ next: 0, operation: '', total: 0 });
+});
+
+test('calculate function should return state for operations', () => {
+  const state = {
+    total: '10',
+    next: '20',
+    operation: '+',
+  };
+  const actual = calculate(state, '+');
+  expect(actual).toEqual({ next: '30', operation: '+', total: new Big(30) });
+});
+
+test('= button returns result', () => {
+  const state = {
+    total: '10',
+    next: '20',
+    operation: '=',
+  };
+  const actual = calculate(state, '+');
+  expect(actual).toEqual({ next: '20', operation: '+', total: new Big(20) });
+});
