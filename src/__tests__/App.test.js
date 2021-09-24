55cb78255292989c1cc86d29eea342feb96b86a0
samroodAli <devsamrood@gmail.com>
2021-09-24 19:15:05 +0530

    Config: Import router into app

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,20 +1,20 @@
 import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
-import App from '../components/App';
+import Calculator from '../components/Calculator';
 
 test('Button snapshot testing', () => {
-  const tree = renderer.create(<App />).toJSON();
+  const tree = renderer.create(<Calculator />).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 test('App renders display component', async () => {
-  const app = render(<App />);
+  const app = render(<Calculator />);
   const display = await app.findAllByTestId('display');
   expect(display).toBeDefined();
 });
 
 test('App renders buttonPanel component', async () => {
-  const app = render(<App />);
+  const app = render(<Calculator />);
   const buttonPanel = await app.findAllByTestId('button');
   expect(buttonPanel).toBeDefined();
 });
