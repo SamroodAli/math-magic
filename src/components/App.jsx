57d869bac25f456dafdbc42d847af65fb41bb5b9
samroodAli <devsamrood@gmail.com>
2021-09-24 20:01:50 +0530

    Routing:Use semantic ui css to use tabs to create SPA

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,74 @@
-import React from 'react';
 import {
   BrowserRouter as Router, Switch, Route, Link,
 } from 'react-router-dom';
-import Calculator from './Calculator';
 
-const App = () => (
-  <div>
-    <Router>
-      <header>
-        <Link to="/">
-          <h1>Math Magic</h1>
-        </Link>
-      </header>
-      <Switch>
-        <Route path="/calculator">
-          <Calculator />
-        </Route>
-        <Route path="/quote">{/* <SearchParams /> */}</Route>
-      </Switch>
-    </Router>
-  </div>
-);
+import React, { Component } from 'react';
+import { Grid, Menu, Segment } from 'semantic-ui-react';
+import Calculator from './Calculator';
+import Quotes from './Quotes';
+import Home from './Home';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { activeItem: 'bio' };
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Router>
+        <header>
+          <Link to="/">
+            <h1>Math Magic</h1>
+          </Link>
+        </header>
+        <Grid>
+          <Grid.Column width={4}>
+            <Menu fluid vertical tabular>
+              <Link to="/">
+                <Menu.Item
+                  name="bio"
+                  active={activeItem === 'bio'}
+                  onClick={this.handleItemClick}
+                />
+              </Link>
+              <Link to="/calculator">
+                <Menu.Item
+                  name="calculator"
+                  active={activeItem === 'calculator'}
+                  onClick={this.handleItemClick}
+                />
+              </Link>
+              <Link to="/quotes">
+                <Menu.Item
+                  name="quotes"
+                  active={activeItem === 'quotes'}
+                  onClick={this.handleItemClick}
+                />
+              </Link>
+
+            </Menu>
+          </Grid.Column>
+
+          <Grid.Column stretched width={12}>
+            <Segment>
+
+              <Switch>
+                <Route path="/" component={Home} />
+                <Route path="/calculator" component={Calculator} />
+                <Route path="/quotes" component={Quotes} />
+              </Switch>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+      </Router>
+    );
+  }
+}
 
 export default App;
--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Quote = () => {
-  const [quote, setQuote] = useState({ content: '', author: '' });
+  const [content, setContent] = useState({ quote: 'Loading', author: '' });
 
   const getQuotes = async () => {
-    const quote = await fetch('https://random-math-quote-api.herokuapp.com/');
-    setQuote(quote);
-    setQuote();
+    const { data } = await axios.get('https://random-math-quote-api.herokuapp.com/');
+    setContent(data);
   };
 
   useEffect(() => {
     getQuotes();
   }, []);
 
+  const { quote, author } = content;
   return (
     <>
       <h2>Math Quotes</h2>
       <p>{quote}</p>
+      <p>{author}</p>
     </>
   );
 };
