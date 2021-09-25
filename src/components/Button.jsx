0f1913dbd8f2e55b230a45311b6e530aa8110590
samroodAli <devsamrood@gmail.com>
2021-09-25 12:58:19 +0530

    Style: Style calculator width and color using props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style/Button.css';
 
-const Button = ({ name, onClick, color }) => (
-  <button data-testid="button" className="button" style={{ backgroundColor: color }} type="button" onClick={() => onClick(name)}>{name}</button>
-);
+const Button = ({
+  name, onClick, color, wide,
+}) => {
+  const style = {
+    backgroundColor: color,
+    width: wide ? '50%' : '25%',
+  };
+  return <button data-testid="button" className="button" style={style} type="button" onClick={() => onClick(name)}>{name}</button>;
+};
 
 Button.defaultProps = {
   color: 'orange',
+  wide: false,
 };
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   color: PropTypes.string,
+  wide: PropTypes.bool,
 };
 
 export default Button;
