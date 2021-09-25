028f2eea729bdec04acf86d4e3b963174c67da75
samroodAli <devsamrood@gmail.com>
2021-09-25 11:35:45 +0530

    Refactor: Refactor ButtonPanel to functional component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style/Display.css';
 
-class Display extends React.PureComponent {
-  render() {
-    const { result } = this.props;
-    return <div data-testid="display" className="display">{result}</div>;
-  }
-}
+const Display = ({ result }) => (
+  <div data-testid="display" className="display">{result}</div>
 
-export default Display;
+);
 
 Display.defaultProps = {
   result: '0',
@@ -18,3 +14,5 @@ Display.defaultProps = {
 Display.propTypes = {
   result: PropTypes.string,
 };
+
+export default Display;
