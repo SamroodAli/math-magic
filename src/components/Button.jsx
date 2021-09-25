4aab0cd782a494125eb273ab1ad5f6f3194f5959
samroodAli <devsamrood@gmail.com>
2021-09-25 12:54:45 +0530

    Style: Using props to style background color of butttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style/Button.css';
 
-const Button = ({ name, onClick }) => (
-  <button className="button" data-testid="button" type="button" onClick={() => onClick(name)}>{name}</button>
+const Button = ({ name, onClick, color }) => (
+  <button data-testid="button" className="button" style={{ backgroundColor: color }} type="button" onClick={() => onClick(name)}>{name}</button>
 );
+
+Button.defaultProps = {
+  color: 'orange',
+};
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  color: PropTypes.string,
 };
 
 export default Button;
