f343fecff4029ccb5210be5b3366c58185b46554
samroodAli <devsamrood@gmail.com>
2021-09-25 13:04:44 +0530

    Test: Test backgroundColor and width of buttons

--- src/__tests__/Button.test.js
+++ src/__tests__/Button.test.js
@@ -17,6 +17,34 @@ test('Button component to call the number with the onClick function', async () =
   expect(onClick).toHaveBeenCalledWith('1');
 });
 
+test('Button component should be wide if wide prop given', async () => {
+  const onClick = jest.fn();
+  const buttonRender = render(<Button name="1" onClick={onClick} wide />);
+  const button = await buttonRender.findByTestId('button');
+  expect(button.style.width).toEqual('50%');
+});
+
+test('Button component should not be wide if wide prop not given', async () => {
+  const onClick = jest.fn();
+  const buttonRender = render(<Button name="1" onClick={onClick} />);
+  const button = await buttonRender.findByTestId('button');
+  expect(button.style.width).toEqual('25%');
+});
+
+test('Button component should be orange if color prop not given', async () => {
+  const onClick = jest.fn();
+  const buttonRender = render(<Button name="1" onClick={onClick} />);
+  const button = await buttonRender.findByTestId('button');
+  expect(button.style.backgroundColor).toEqual('orange');
+});
+
+test('Button component should be given color if color prop given', async () => {
+  const onClick = jest.fn();
+  const buttonRender = render(<Button name="1" onClick={onClick} color="grey" />);
+  const button = await buttonRender.findByTestId('button');
+  expect(button.style.backgroundColor).toEqual('grey');
+});
+
 test('Button snapshot testing', () => {
   const tree = renderer
     .create(<Button name="1" onClick={jest.fn()} />)
