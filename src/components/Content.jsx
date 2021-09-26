7dcd3493fff3922a4dbbef60e48c58424d53bc24
samroodAli <devsamrood@gmail.com>
2021-09-26 15:57:36 +0530

    Style: Update button styles to reflect theme using context

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,24 +2,28 @@ import PropTypes from 'prop-types';
 import {
   Button, Image, Icon,
 } from 'semantic-ui-react';
+import useStyle from '../hooks/useStyle';
 
 const Content = ({
   quote, author, authorImg, getQuotes,
-}) => (
-  <>
-    <div style={{ height: '400px' }}>
-      <Image height="400px" circular src={authorImg} alt={author} centered />
-    </div>
-    <Button animated onClick={getQuotes} style={{ margin: '1rem' }} color="black">
-      <Button.Content visible>Next</Button.Content>
-      <Button.Content hidden>
-        <Icon name="arrow right" />
-      </Button.Content>
-    </Button>
-    <h3 data-testid="quote">{quote}</h3>
-    <h4 data-testid="author">{author}</h4>
-  </>
-);
+}) => {
+  const { mainBackground, textColor } = useStyle();
+  return (
+    <>
+      <div style={{ height: '400px' }}>
+        <Image height="400px" circular src={authorImg} alt={author} centered />
+      </div>
+      <Button animated onClick={getQuotes} style={{ margin: '1rem', ...mainBackground, ...textColor }} color="black">
+        <Button.Content visible>Next</Button.Content>
+        <Button.Content hidden>
+          <Icon name="arrow right" />
+        </Button.Content>
+      </Button>
+      <h3 data-testid="quote">{quote}</h3>
+      <h4 data-testid="author">{author}</h4>
+    </>
+  );
+};
 
 Content.propTypes = {
   quote: PropTypes.string.isRequired,
